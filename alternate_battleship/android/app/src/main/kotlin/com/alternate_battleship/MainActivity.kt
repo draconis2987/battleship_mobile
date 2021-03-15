5746cacd547b00679448090ccb1a8a3a83d6482a
drake <draconisrobinson@gmail.com>
2021-03-15 15:33:41 -0600

    app initial commit

---
+++ alternate_battleship/android/app/src/main/kotlin/com/alternate_battleship/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.alternate_battleship
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ alternate_battleship/lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flame/game.dart';
+import 'package:flame/flame.dart';
+
+void main() => runApp(BattleShipMain().widget);
+
+class BattleShipMain extends BaseGame {
+
+  @override
+  void render(Canvas canvas) {}
+
+  @override
+  void update(double t) {
+
+  }
+
+}
